a2a6e0f314bbd7d88f73c95a1954ff779f602027
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-11-20 18:19:56 -0300

    Solution with interface TaxService

--- src/services/BrazilTaxService.java
+++ src/services/BrazilTaxService.java
@@ -1,8 +1,9 @@
 package services;
 
-public class BrazilTaxService {
+public class BrazilTaxService implements TaxService {
 
-    public double tax(double amount){
+    @Override
+    public double tax(double amount) {
         if(amount <= 100){
             return amount * 0.2;
         } else{
